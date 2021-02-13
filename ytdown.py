c0bbaa27f32b6c97f44d0ea1ceb74d2a8702c2f3
Rishi Kumar <rishikumar.jazz098@gmail.com>
2021-02-13 20:04:54 +0530

    added themes

--- ytdown.py
+++ ytdown.py
@@ -3,19 +3,29 @@ import tkinter as tk
 from tkinter import ttk
 from threading import Thread
 from tkinter import font
+from ttkthemes import ThemedStyle
 
 
 win = tk.Tk()
 win.title("Youtube Downloader")
-# win.minsize(400, 225)
-win.maxsize(1920, 1080)
 win.resizable(False, False)
 win.iconbitmap("youtube.ico")
 
+style = ThemedStyle(win)
+style.set_theme("equilux")
 
-style = ttk.Style(win)
+
+myFont = font.Font(family="Comic Sans MS")
+
+style_main = ttk.Style(win)
 font = font.Font(family="Comic Sans MS", size=10, weight="bold")
-style.configure("TButton", font=font)
+style_main.configure("TButton", font=font)
+
+
+main_frame = ttk.LabelFrame(win)
+main_frame.grid(row=0, column=0, padx=10, pady=10)
+main_frame["padding"] = [20]
+# main_frame["font"] = font
 
 
 def startDownloadingThread(file_handler):
@@ -30,10 +40,10 @@ def startDownloading(file_handler):
     elif radSel == 1:
         abc = file_handler.getbestvideo(preftype="mp4", ftypestrict=False)
     elif radSel == 2:
-        abc = file_handler.getbestaudio(preftype="m4a", ftypestrict=False)
+        abc = file_handler.getbestaudio()
     abc.download()
     result_label.configure(
-        text=f"{file_handler.title}\nDuration :  {file_handler.duration}\nblah blah\n\n\nDownloaded"
+        text=f"{file_handler.title}\nDuration :  {file_handler.duration}\n\n\nDownloaded"
     )
 
 
@@ -42,27 +52,35 @@ def SearchFor():
     if url == "":
         result_label.configure(text="Seems like you haven't entered an link yet")
     else:
-        # result_label.configure(text=url)
         try:
             file_handler = pafy.new(url)
             video_title = file_handler.title
             video_duration = file_handler.duration
-            # abc = file_handler.getbest()
-            # abc.download()
             startDownloadingThread(file_handler)  # Download in a separate thread
 
-            # list_of_streams = file_handler.allstreams
             result_label.configure(
-                text=f"{video_title}\nDuration :  {video_duration}\nblah\nblah\nblah...\n\n\nDownload in progress"
+                text=f"{video_title}\nDuration :  {video_duration}\n\n\nDownload in progress"
             )
         except:
             result_label.configure(text="Please enter a valid youtube link")
 
 
-main_frame = ttk.LabelFrame(win, text="Welcome to my youtube downloader")
-main_frame.grid(row=0, column=0, padx=10, pady=10)
-main_frame["padding"] = [20, 0]
-# main_frame["font"] = font
+def change_theme(a):
+    theme = selected_theme.get()
+    result_label.configure(text="Changed theme to " + theme)
+    style.set_theme(theme)
+    if theme == "scidpink":
+        win.configure(background="#F72D60")
+    elif theme == "scidmint":
+        win.configure(background="#7EAF51")
+    elif theme == "scidblue":
+        win.configure(background="#41A5E1")
+    elif theme == "black":
+        win.configure(background="#323232")
+    elif theme == "kroc":
+        win.configure(background="#FFA66A")
+    else:
+        win.configure(background="#f05b5b")
 
 
 url_label = ttk.Label(
@@ -70,47 +88,62 @@ url_label = ttk.Label(
     text="Enter youtube link here:",
     font=("Comic Sans MS", 10, "bold italic"),
 )
-# font=("Comic Sans MS", 10),
 url_label.grid(row=0, column=0, padx=5, pady=10, sticky="W")
+
+
+selected_theme = tk.StringVar()
+number_field = ttk.Combobox(
+    main_frame,
+    width=20,
+    textvariable=selected_theme,
+    state="readonly",
+)
+number_field["values"] = (
+    "xpnative",
+    "kroc",
+    "itft1",
+    "blue",
+    "equilux",
+    "scidpink",
+    "scidblue",
+    "scidmint",
+    "black",
+)
+number_field.current(4)
+number_field.grid(row=0, column=2, pady=10, sticky="E")
+number_field.bind("<<ComboboxSelected>>", change_theme)
 
 
 entered_url = tk.StringVar()
 entry_box = ttk.Entry(main_frame, width=100, textvariable=entered_url)
-entry_box.grid(row=1, column=0, sticky="WE", columnspan=3)
+entry_box.grid(row=1, column=0, padx=5, pady=10, sticky="WE", columnspan=3)
 
 
 radVar = tk.IntVar()
-rad0 = tk.Radiobutton(
+rad0 = ttk.Radiobutton(
     main_frame,
     text="VIDEO",
     variable=radVar,
     value=0,
-    font=("Comic Sans MS", 10, "bold italic"),
 )
-rad1 = tk.Radiobutton(
+rad1 = ttk.Radiobutton(
     main_frame,
     text="VIDEO ONLY",
     variable=radVar,
     value=1,
-    font=("Comic Sans MS", 10, "bold italic"),
 )
-rad2 = tk.Radiobutton(
+rad2 = ttk.Radiobutton(
     main_frame,
     text="AUDIO ONLY",
     variable=radVar,
     value=2,
-    font=("Comic Sans MS", 10, "bold italic"),
 )
 rad0.grid(row=2, column=0, sticky="W")
 rad1.grid(row=2, column=1, sticky="W")
 rad2.grid(row=2, column=2, sticky="E")
 
 
-download_button = ttk.Button(
-    main_frame,
-    text="Download",
-    command=SearchFor,
-)
+download_button = ttk.Button(main_frame, text="Download", command=SearchFor)
 download_button.grid(row=3, column=0, pady=10, columnspan=3)
 
 result_label = ttk.Label(
@@ -121,9 +154,7 @@ result_label = ttk.Label(
 result_label.grid(row=4, column=0, rowspan=10, columnspan=3)
 entry_box.focus()
 win.configure(background="#f05b5b")
-win.attributes("-alpha", 0.9)
-win.mainloop()
+# win.attributes("-alpha", 0.9)  # transparency
 
-# url = pyperclip.paste()
-# if (url == ""):
-#    print("empty string")
+
+win.mainloop()
